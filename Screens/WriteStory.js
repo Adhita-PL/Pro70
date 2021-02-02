2cc09e7d20cfb0fc4773c0e4b77a41aae1d24662
Adhita <70697802+Adhita-PL@users.noreply.github.com>
2021-02-02 14:10:29 +0530

    Update WriteStory.js

--- Screens/WriteStory.js
+++ Screens/WriteStory.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text, View, TextInput, TouchableOpacity, StyleSheet  } from 'react-native';
 import { Header } from 'react-native-elements';
 
-export default class Insta extends React.Component{
+export default class WriteStory extends React.Component{
     render() {
         return(
             <View style = {{flex:1, justifyContent: 'center', alignItems: 'center'}}>
